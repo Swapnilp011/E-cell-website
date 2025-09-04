3c981e2e9d13fcd8ecb6cedc7c9409215a812df0
Swapnil Pawar <swapnildpawar011@gmail.com>
2025-09-04 08:35:30 +0000

    add the team option in the header

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,6 +7,7 @@ import { Eureka } from '@/components/sections/Eureka';
 import { OpenSourceFest } from '@/components/sections/OpenSourceFest';
 import { OtherInitiatives } from '@/components/sections/OtherInitiatives';
 import { LeadershipVoice } from '@/components/sections/LeadershipVoice';
+import { Team } from '@/components/sections/Team';
 
 export default function Home() {
   return (
@@ -21,6 +22,7 @@ export default function Home() {
           <OpenSourceFest />
         </div>
         <OtherInitiatives />
+        <Team />
         <LeadershipVoice />
       </main>
       <Footer />
